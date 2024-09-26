7f4de71d616e8718a8aa5963532f583b209b1bff
unknown <Administrateur@PC3-A06.formationara.local>
2024-09-26 13:34:38 +0200

    adding player horizontal movements

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerMovements : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject player;
+    [SerializeField] private float sidewayForce = 5f;
+
+    private float horizontalAxis = 0f;
+
+    private void Update()
     {
-        
+        horizontalAxis = Input.GetAxis("Horizontal");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        player.transform.position += new Vector3(horizontalAxis * sidewayForce * Time.deltaTime, 0, 0);
     }
 }
